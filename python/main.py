86d6b3ac171d50c4af779fc24df77dc6ef04a3ef
AdrKacz <adrien.kaczmarek@gmail.com>
2021-10-08 10:35:40 +0200

    Graphics, color done

--- python/main.py
+++ python/main.py
@@ -3,29 +3,40 @@ import time
 
 class Environment:
     def __init__(self, n, m):
-        self.grid = [[0 for j in range(m)] for i in range(n)]
+        self.grid = [[None for j in range(m)] for i in range(n)]
+
+    def map_color(self, cell):
+        if cell == 'A':
+            return 1
+        elif cell == 'B':
+            return 2
+        elif cell == 'O':
+            return 3
+        else:
+            return 0
 
     def display(self):
         for i, row in enumerate(self.grid):
             for j, cell in enumerate(row):
-                if cell == 1:
-                    stdscr.addstr(i, 2 * j, str(cell), curses.color_pair(1))
-                else:
-                    stdscr.addstr(i, 2 * j, str(cell))
-                stdscr.refresh()
+                if cell:
+                    stdscr.addstr(i, 2 * j, str(cell), curses.color_pair(self.map_color(cell)))
+                    stdscr.refresh()
 
 
 if __name__ == '__main__':
     stdscr = curses.initscr()
     curses.start_color()
     curses.use_default_colors()
-    curses.init_pair(1, curses.COLOR_RED, 0)
+    curses.init_pair(0, 0, -1)
+    curses.init_pair(1, 1, -1)
+    curses.init_pair(1, 2, -1)
+    curses.init_pair(1, 3, -1)
     curses.noecho()
     curses.cbreak()
 
     env = Environment(5, 5)
     for i in range(5):
-        env.grid[i][i] = 1
+        env.grid[i][i] = 'A'
         env.display()
         time.sleep(0.5)
 
